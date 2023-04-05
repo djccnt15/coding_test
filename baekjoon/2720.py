42ea9fc4bde5e8d50b18b399f200813ae79c1267
djccnt15 <djccnt15@gmail.com>
2023-04-06 00:22:32 +0900

    update BOJ solution

---
+++ baekjoon/2720.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    c = int(input())
+    a = []
+    c, r = divmod(c, 25)
+    a.append(c)
+    c, r = divmod(r, 10)
+    a.append(c)
+    a.extend(divmod(r, 5))
+    print(' '.join([str(v) for v in a]))
