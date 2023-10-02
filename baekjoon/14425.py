1eebc7c57798671246482c54ea299abc8ea7948f
djccnt15 <djccnt15@gmail.com>
2023-10-02 19:14:35 +0900

    minor fixes for code convention

--- baekjoon/14425.py
+++ baekjoon/14425.py
@@ -1,6 +1,9 @@
 import sys
+
 input = sys.stdin.readline
+
 n, m = [int(x) for x in input().split()]
 s = {input() for _ in range(n)}
 m = [input() for _ in range(m)]
+
 print(sum(1 for x in m if x in s))
--- baekjoon/2751.py
+++ baekjoon/2751.py
@@ -1,4 +1,6 @@
 import sys
+
 input = sys.stdin.readline
+
 for x in sorted([int(x) for x in [int(input()) for _ in range(int(input()))]]):
     print(x)
